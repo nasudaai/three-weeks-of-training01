4c96abc2a9e656e22b7325c0eb18f57424294b60
nasuda <nasuda908@gmail.com>
2026-02-18 16:50:52 +0900

    add std update

--- firstweek/std.mjs
+++ firstweek/std.mjs
@@ -1,4 +1,9 @@
+import { writeFileSync } from 'node:fs'
+console.log(writeFileSync);
+
 process.stdin.on("data", (chunk) => {
   console.log(chunk);
   process.stdout.write(chunk);
+
+  writeFileSync("./log.md", chunk);
 });

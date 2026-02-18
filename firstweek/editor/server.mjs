a578ca2c6d558f5e56a8da4f3032de7af2775e5e
nasuda <nasuda908@gmail.com>
2026-02-18 16:54:40 +0900

    add server

--- firstweek/editor/server.mjs
+++ firstweek/editor/server.mjs
@@ -1,3 +1,11 @@
 import { createServer } from 'node:http';
 
 console.log(createServer);
+
+const server = createServer((req, res) => {
+  res.end("html");
+});
+
+server.listen(3000, () => {
+  console.log("listen on http://localhost:3000")
+});

08d683818cbf58e3c8935117b1df0c594112225c
nasuda <nasuda908@gmail.com>
2026-02-20 16:19:54 +0900

    add sh

--- daily/create_daily.sh
+++ daily/create_daily.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-echo "test"
+#echo "test"
+
+filename="$(date "+%Y-%m-%d").mjs"
+#echo $filename
+
+
+mode=$1
+
+if [ $mode = "make" ]; then
+  echo "make"
+fi
+
+if [ $mode = "mv" ]; then
+  echo "mv"
+
+  mv $2 $filename
+fi

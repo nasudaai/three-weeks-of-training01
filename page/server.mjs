d307dc2ecbe4c9bdabb30e202b0506097db04186
nasuda <nasuda908@gmail.com>
2026-02-22 18:13:00 +0900

    add page

---
+++ page/server.mjs
@@ -0,0 +1,10 @@
+import { createServer } from 'node:http';
+console.log(createServer);
+
+const server = createServer((req, res) => {
+  res.end("html");
+});
+
+server.listen(3000, () => {
+  console.log("listen on http://localhost:3000");
+});

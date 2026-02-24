a62188a946b3f47128ae006c11a8857ae9aa7708
nasuda <nasuda908@gmail.com>
2026-02-24 09:29:19 +0900

    add obj

--- second-week/script.mjs
+++ second-week/script.mjs
@@ -1,6 +1,15 @@
 const args = process.argv.slice(2);
 console.log(args);
 
+const obj = {
+  key: "value"
+};
+
+let {key} = obj;
+console.log(key);
+
+
+
 function write() {
 
   process.stdout.write("str");

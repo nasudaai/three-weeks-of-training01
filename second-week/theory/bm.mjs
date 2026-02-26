cd4fcb93b712946a5e1a087e8ec3880d548353cf
nasuda <nasuda908@gmail.com>
2026-02-26 23:54:39 +0900

    add degree

--- second-week/theory/bm.mjs
+++ second-week/theory/bm.mjs
@@ -1,2 +1,15 @@
 //composition
 const chordProgression = ["Bm", "A"];
+
+const majorDegNum = ["6", "5"];
+const minor = [1, 7];
+
+let deg1 = majorDegNum[0];
+let deg2 = majorDegNum[1];
+
+let degree = {
+  [deg1]: chordProgression[0],
+  [deg2]: chordProgression[1]
+};
+
+console.log(deg1 + ": " + degree[deg1], deg2 + ":" +  degree[deg2]);

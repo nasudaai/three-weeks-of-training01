6023f1463c59bbf0fec5f3bf2c1eb412f8c9b5f2
nasuda <nasuda908@gmail.com>
2026-02-27 11:02:09 +0900

    add module

--- second-week/src/main.mjs
+++ second-week/src/main.mjs
@@ -1,5 +1,8 @@
+import {write} from './out.mjs';
+
 function main() {
   console.log("test");
+  write("string");
 };
 
 main();
---
+++ second-week/src/out.mjs
@@ -0,0 +1,5 @@
+function write(str) {
+  process.stdout.write(str);
+};
+
+export {write};

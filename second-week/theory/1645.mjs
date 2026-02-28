ef25c3ed1007067f68e6c1bcfe77c3c3b36e2d57
nasuda <nasuda908@gmail.com>
2026-02-28 21:30:03 +0900

    add

---
+++ second-week/theory/1645.mjs
@@ -0,0 +1,15 @@
+const basic = [1, 6, 4, 5];
+console.log(basic);
+
+const m = [6, 3];
+
+const newArr = [];
+
+newArr.push(m);
+console.log(newArr);
+
+
+newArr.push(basic[2]);
+newArr.push(basic[3]);
+
+console.log(newArr);
